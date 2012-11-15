e1b29e7a9438c44ea76cd5281cea69241f9999db
Joshua Durkee <joshua.durkee@oit.edu>
2012-11-15 12:22:15 -0800

    Finished pathfinding setup
    I finished setting up the pathfinding framework in the project.  Also,
    the framework wanted to reorganize4 files to allow for use with
    javascript.  I don't plan to use javascript, but hey, it just moves
    things, so it shouldn't hurt.

---
+++ Scripts/GraphUpdaterScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class GraphUpdaterScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		InvokeRepeating("UpdateGraph",0,1);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+	
+	public void UpdateGraph()
+	{
+		GetComponent<GraphUpdateScene>().Apply ();
+	}
+}
