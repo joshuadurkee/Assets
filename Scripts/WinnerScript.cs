2dfd3b4f66eeabf7302cf1729068323b834d4f6c
Joshua Durkee <joshua.durkee@oit.edu>
2013-04-26 11:05:57 -0700

    Finished level & added win ability
    Finished building level for demonstration of game.  Performance issues
    will need to be addressed if my machine is to be used for demoing the
    game.
    Also added the ability to win.  At the end of the level, a trigger with
    a blue particle effect sends the game to the main menu.  No win message
    yet, but will need to add one to avoid confusion.

---
+++ Scripts/WinnerScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class WinnerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			Application.LoadLevel(0);
+		}
+	}
+}
